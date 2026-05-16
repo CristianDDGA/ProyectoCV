def74f3ef2517b2d75e4a4edc29ceef20c1edddc
CristianAngo <cango7999@uta.edu.ec>
2026-05-16 14:12:33 -0500

    Primer Commit del proyecto

---
+++ src/main/java/com/mycompany/proyectocv/ProyectoCV.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectocv;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class ProyectoCV {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
